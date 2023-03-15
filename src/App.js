0a50cc80ac54baa9a98ee969872c984e0f780238
ktadique <117470787+ktadique@users.noreply.github.com>
2023-03-15 14:50:36 +0000

    created default books JSON file, starting on nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React from 'react';
+import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import './assets/styles/App.scss';
-
+import "./assets/styles/App.scss";
 
 function App() {
-  return (
-    <div className="App">
-
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function Navigation() {}
+
+export default Navigation;
