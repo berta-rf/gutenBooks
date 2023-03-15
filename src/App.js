4f3b491bedfb97a573d8d0a0d9a34b16df15d481
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2023-03-15 14:30:52 +0000

    Create assets folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import './App.scss';
+import './assets/styles/App.scss';
 
 
 function App() {
