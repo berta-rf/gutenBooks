8b30aab32cb6a9cf3598fd0c8951117f4eccdd31
ktadique <117470787+ktadique@users.noreply.github.com>
2023-03-15 17:07:40 +0000

    created basic navigation feature plus drawer function for smaller displays

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Navigation from "./components/Navigation";
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import "./assets/styles/App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/system";
+import {
+  AppBar,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { styled, useTheme } from "@mui/material/styles";
 
-function Navigation() {}
+const links = ["Bookshelf", "Your Reviews"];
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const drawerWidth = 350;
+
+function Navigation() {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1, marginBottom: 3 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontWeight: 700,
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              GutenBooks
+            </Typography>
+            <Box
+              sx={{
+                flexGrow: 1,
+                flexDirection: "row",
+                display: { xs: "flex", md: "none" },
+              }}
+            >
+              <IconButton
+                size="large"
+                aria-label="hamburger icon"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleDrawerOpen}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {links.map((page) => (
+                <Button
+                  key={page}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": { width: drawerWidth, boxSizing: "border-box" },
+        }}
+        variant="temporary"
+        anchor="left"
+        onClose={() => {
+          handleDrawerClose();
+        }}
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>x</IconButton>
+        </DrawerHeader>
+        {links.map((page) => (
+          <Button
+            key={page}
+            to={`/${page}`}
+            sx={{ my: 2, color: "black", display: "block" }}
+          >
+            {page}
+          </Button>
+        ))}
+      </Drawer>
+    </div>
+  );
+}
 
 export default Navigation;
