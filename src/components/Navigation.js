43a7cab77e114d7242960c439016654c025caa4e
Berta Romano Fernandez <116377561+berta-rf@users.noreply.github.com>
2023-03-18 10:35:50 +0000

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
-import SearchBook from "./SearchBook";
+//import SearchBook from "./SearchBook";
 
 //MUI
 import {
