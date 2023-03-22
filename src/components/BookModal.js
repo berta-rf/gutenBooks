c9523ea75f68035be1f0e013f4c86c347f2c154f
chinapicke <chinapickering@hotmail.co.uk>
2023-03-22 23:56:25 +0700

    media queries added for modals

--- src/components/BookModal.js
+++ src/components/BookModal.js
@@ -6,9 +6,9 @@ import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useSpring, animated } from "@react-spring/web";
-import axios from "axios";
-import { useState } from "react";
-import "../assets/styles/modal.scss";
+import axios from 'axios'
+import { useState } from 'react'
+import '../assets/styles/modal.scss'
 const Fade = React.forwardRef(function Fade(props, ref) {
   const {
     children,
@@ -52,8 +52,6 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 700,
-  height: 450,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -63,90 +61,59 @@ function BookModal(props) {
   const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
   const [description, setDescription] = useState("");
-  const handleOpen = () => {
-    setOpen(true);
-    // console.log('Calling descript', props.title);
-    axios
-      .get(
-        `https://www.googleapis.com/books/v1/volumes?q=${props.title}&key=AIzaSyDxKZhFCI9K0aCExPM-D-bIDvZ0UAmylow`
-      )
-      .then((res) => {
-        let data = res.data.items[0].volumeInfo.description;
-        // console.log(data);;
-        // .catch(err => console.log(err))
-        setDescription(data);
-        // console.log("Description" + description)
-      });
-  };
+  const handleOpen = () => {setOpen(true)
+      // console.log('Calling descript', props.title);
+      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${props.title}&key=AIzaSyDxKZhFCI9K0aCExPM-D-bIDvZ0UAmylow`)
+        .then(res => {
+          let data = (res.data.items[0].volumeInfo.description);
+          // console.log(data);;
+          // .catch(err => console.log(err))
+          setDescription(data)
+          // console.log("Description" + description)
+        })
+  }
   return (
     <div>
-      <Button
-        className={`descriptionBtn ${"bookCardButton"}`}
-        onClick={handleOpen}
-      >
-        Description
-      </Button>
-      <Modal
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
-      >
-        <Box sx={style}>
-          <Typography
-            id="modalTitle"
-            className="modalInfo"
-            variant="h5"
-            component="h1"
-          >
-            Title:
-          </Typography>
-          <p className="modalText">{props.title}</p>
-          {/* <Typography id='modalTitle' variant="h5" component="h2">
+      <Button className='descriptionBtn'onClick={handleOpen}>Description</Button>
+        <Modal
+          aria-labelledby="spring-modal-title"
+          aria-describedby="spring-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              TransitionComponent: Fade,
+            },
+          }}
+        >
+          <Box className='modalBox' sx={style}>
+            <Typography id='modalTitle' className='modalInfo' variant="h5" component="h1">
+              Title:
+            </Typography>
+            <p className='modalText'>{props.title}</p>
+            {/* <Typography id='modalTitle' variant="h5" component="h2">
               Author: {props.author}
             </Typography> */}
-          <Typography
-            id="modalLanguage"
-            className="modalInfo"
-            variant="h5"
-            component="h1"
-          >
-            Language:
-          </Typography>
-          <p className="modalText">{props.language}</p>
-          <Typography
-            id="modalDescription"
-            className="modalInfo"
-            variant="h5"
-            component="h1"
-          >
-            Description:
-          </Typography>
-          <p className="modalText limit">{description}</p>
-          <Typography
-            id="modalSubjects"
-            className="modalInfo"
-            variant="h5"
-            component="h1"
-          >
-            Genre:
-          </Typography>
-          <div className="modalGenre">
+            <Typography id="modalLanguage"  className='modalInfo' variant="h5" component="h1">
+              Language:
+            </Typography>
+            <p className='modalText'>{props.language}</p>
+            <Typography id="modalDescription"  className='modalInfo' variant="h5" component="h1">
+              Description:
+            </Typography>
+            <p className='modalText limit'>{description}</p>
+            <Typography id="modalSubjects"  className='modalInfo'  variant="h5" component="h1">
+              Genre:
+            </Typography>
+            <div className='modalGenre'>
             {props.subjects.map((subject, index) => (
-              <button className="subjectText" key={index}>
-                {subject}
-              </button>
-            ))}
-          </div>
-        </Box>
-      </Modal>
+                <button className='subjectText' key={index}>{subject}</button>
+              ))}
+            </div>
+          </Box>
+        </Modal>
     </div>
   );
 }
